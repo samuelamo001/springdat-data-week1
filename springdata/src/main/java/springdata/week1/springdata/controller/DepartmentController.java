78910c012b38f9a411c86bcd2b3d794dbba35979
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-08-29 23:59:55 +0000

    added transactions

---
+++ springdata/src/main/java/springdata/week1/springdata/controller/DepartmentController.java
@@ -0,0 +1,22 @@
+package springdata.week1.springdata.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import springdata.week1.springdata.dto.DepartmentDTO;
+import springdata.week1.springdata.entities.Department;
+import springdata.week1.springdata.service.DepartmentService;
+
+@RestController
+@RequiredArgsConstructor
+
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    @PostMapping
+    public Department createDepartment(@RequestBody DepartmentDTO department) {
+        return departmentService.createDepartment(department);
+    }
+}
--- springdata/src/main/java/springdata/week1/springdata/service/DepartmentService.java
+++ springdata/src/main/java/springdata/week1/springdata/service/DepartmentService.java
@@ -1,7 +1,12 @@
 package springdata.week1.springdata.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import springdata.week1.springdata.dto.DepartmentDTO;
 import springdata.week1.springdata.entities.Department;
 import springdata.week1.springdata.entities.Doctor;
@@ -11,27 +16,39 @@ import springdata.week1.springdata.repository.DoctorRepository;
 import java.util.List;
 
 
+@RequiredArgsConstructor
 @Service
 public class DepartmentService {
 
     private final DepartmentRepository departmentRepository;
     private final DoctorRepository doctorRepository;
+    private final PlatformTransactionManager transactionManager;
 
-
-    public DepartmentService(DepartmentRepository departmentRepository, DoctorRepository doctorRepository) {
-        this.departmentRepository = departmentRepository;
-        this.doctorRepository = doctorRepository;
-    }
     public Department createDepartment(DepartmentDTO departmentDTO) {
-        Doctor doctor = doctorRepository.findById(departmentDTO.getDirector()).orElseThrow(()-> new EntityNotFoundException("Not found"));
-        Department department = Department.builder()
-                .name(departmentDTO.getName())
-                .code(departmentDTO.getCode())
-                .building(departmentDTO.getBuilding())
-                .director(doctor)
-                .build();
 
-        return departmentRepository.save(department);
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setName("departmentCreationTransactionDefinition");
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = transactionManager.getTransaction(def);
+        try {
+            Doctor doctor = doctorRepository.findById(departmentDTO.getDirector())
+                    .orElseThrow(() -> new EntityNotFoundException("Director not found"));
+
+            Department department = Department.builder()
+                    .name(departmentDTO.getName())
+                    .code(departmentDTO.getCode())
+                    .building(departmentDTO.getBuilding())
+                    .director(doctor)
+                    .build();
+
+
+            departmentRepository.save(department);
+            transactionManager.commit(status);
+            return department;
+
+        } catch (Exception ex) {
+            transactionManager.rollback(status);
+            throw ex;          }
     }
 
     public List<Department> getAllDepartments() {
