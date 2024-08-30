e0b785668c4ae7f59c9670e8d87729dd4a820402
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-08-30 00:28:02 +0000

    added caching

--- springdata/src/main/java/springdata/week1/springdata/controller/DepartmentController.java
+++ springdata/src/main/java/springdata/week1/springdata/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package springdata.week1.springdata.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springdata.week1.springdata.dto.DepartmentDTO;
 import springdata.week1.springdata.entities.Department;
@@ -10,7 +11,7 @@ import springdata.week1.springdata.service.DepartmentService;
 
 @RestController
 @RequiredArgsConstructor
-
+@RequestMapping("/api/v1/department")
 public class DepartmentController {
 
     private final DepartmentService departmentService;
