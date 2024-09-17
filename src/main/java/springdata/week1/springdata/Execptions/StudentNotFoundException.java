922075b42f26971178e6a78475b8d6334c5fbfbd
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-09-17 21:04:57 +0000

    update: intial project

--- src/main/java/springdata/week1/springdata/Execptions/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package springdata.week1.springdata.Execptions;
-
-public class StudentNotFoundException extends RuntimeException{
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
